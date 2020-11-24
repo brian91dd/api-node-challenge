b9bb350f4962e11619779ae016fd6c439b9b5303
Brian <briandediego@gmail.com>
2020-11-24 12:39:04 -0300

    Get articles by tag tests

--- src/tests/articles.test.js
+++ src/tests/articles.test.js
@@ -29,7 +29,14 @@ const article1EditDifferentUser = {
 const article2 = {
   title: 'Article 2 test title',
   text: 'Velit imperdiet ultrices tempor montes rhoncus bibendum.',
-  tags: ['ultrices', 'rhoncus'],
+  tags: ['lorem', 'rhoncus'],
+};
+
+const article3 = {
+  userId: '5fbd10e9ae72de60320df3a1',
+  title: 'Article 2 test title',
+  text: 'Velit imperdiet ultrices tempor montes rhoncus bibendum.',
+  tags: ['lorem', 'rhoncus'],
 };
 
 beforeAll(async () => {
@@ -52,15 +59,23 @@ afterAll(async () => {
   await mongoose.connection.close();
 });
 
-test('Should create a new article', (done) => {
-  request(app)
+test('Should create two new articles', async () => {
+  await request(app)
     .post('/v1/articles')
     .send(article1)
     .then((res) => {
       expect(res.body).toMatchObject(article1);
 
       article1._id = res.body._id;
-      done();
+    });
+
+  await request(app)
+    .post('/v1/articles')
+    .send(article3)
+    .then((res) => {
+      expect(res.body).toMatchObject(article2);
+
+      article2._id = res.body._id;
     });
 });
 
@@ -93,6 +108,51 @@ test('Should return error when updating article from different user', (done) => 
     .then((res) => {
       expect(res.body.message).toEqual('Can\'t edit article from different user');
 
+      done();
+    });
+});
+
+test('Should return the articles with a specific tag', (done) => {
+  request(app)
+    .get('/v1/articles')
+    .query({ tags: [article3.tags[1]] })
+    .then((res) => {
+      expect(res.body.length).toBe(1);
+      expect(res.body[0].title).toEqual(article3.title);
+
+      done();
+    });
+});
+
+test('Should return the articles with multiple tags', (done) => {
+  request(app)
+    .get('/v1/articles')
+    .query({ tags: article3.tags })
+    .then((res) => {
+      expect(res.body.length).toBe(2);
+
+      done();
+    });
+});
+
+test('Should not return any articles when tag doesn\'t match', (done) => {
+  request(app)
+    .get('/v1/articles')
+    .query({ tags: ['noArticle'] })
+    .then((res) => {
+      expect(res.body.length).toEqual(0);
+
+      done();
+    });
+});
+
+test('Should return any articles with specific title and the user populated', (done) => {
+  request(app)
+    .get('/v1/articles')
+    .query({ title: article1Edit.title })
+    .then((res) => {
+      expect(res.body.length).toEqual(1);
+      expect(res.body[0].userId.name).toBe('test user');
       done();
     });
 });
