5a260093cbc8d7e43c6f9904a1da8327882f2be6
Brian <briandediego@gmail.com>
2020-11-25 17:03:31 -0300

    Added sanitization (extremely important)
    Also fix a swagger item

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const swaggerUi = require('swagger-ui-express');
+const mongoSanitize = require('express-mongo-sanitize');
 
 const envFile = process.env.NODE_ENV ? `.env.${process.env.NODE_ENV}` : '.env';
 dotenv.config({ path: path.resolve(__dirname, `../${envFile}`) });
@@ -21,6 +22,7 @@ mongoose.connect(process.env.MONGODB_HOST, {
 });
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use(mongoSanitize());
 app.use(bodyParser.json());
 app.use('/v1', authMiddleware.handler, routesV1);
 app.use('/*', (req, res) => res.status(404).send('Not Found'));
